12caaa3ccf8bed5a6a042a0daded7f5de7d87353
Steve Cai <steve.cai@morningstar.com>
2017-10-11 15:32:23 +0800

    commit一些

--- source/pages/latest/latest.js
+++ source/pages/latest/latest.js
@@ -1,4 +1,4 @@
-// pages/nearby/nearby.js
+// pages/latest/latest.js
 Page({
 
   /**
--- source/pages/map/map.js
+++ source/pages/map/map.js
@@ -1,18 +1,50 @@
 // pages/map/map.js
+var QQMapWX = require('../../libs/qqmap/qqmap-wx-jssdk.js');
+var qqmapsdk;
 Page({
-
+  mapCtx:null,
   /**
    * 页面的初始数据
    */
   data: {
-  
+    lat: 22.531585,
+    lng: 114.055939,
+    markers:[]
   },
-
+  regionchange(e) {
+    if(e.type=="end"){
+    var that=this;
+    this.mapCtx.getCenterLocation({
+      success: function (res) {
+        that.setData({ lat: res.latitude, lng: res.longitude});
+        //console.log(that.data.lat);
+        //console.log(that.data.lng);
+        //that.loadMarkersFromCenter();
+        that.mapCtx.moveToLocation();
+      }
+    });
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.createMapContext("map4select");
+    qqmapsdk = new QQMapWX({
+      key: 'IDVBZ-TSAKD-TXG43-H442I-74KVK-6LFF5'
+    });
+    var that=this;
+    this.mapCtx = wx.createMapContext("map4select");
+    wx.getLocation({
+      success:function(res){
+        //console.log(JSON.stringify(res));
+        that.setData({
+          lat: res.latitude,
+          lng: res.longitude
+        });
+        that.mapCtx.moveToLocation();
+        that.loadMarkersFromCenter();
+      }
+    });
   },
 
   /**
@@ -62,5 +94,34 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  setMarker(marker){
+    this.data.markers.push(marker);
+  },
+  loadMarkersFromCenter(){
+    this.loadMarkers(this.data.lat,this.data.lng);
+  },
+  loadMarkers(center_lat,center_lng){
+    var markers = [];
+    for (var i = 0; i < 15; i++) {
+      var lat = (((Math.random() * 50) % 2 > 1 ? 1 : -1) * Math.random() / 1000).toFixed(6);
+      var lng = (((Math.random() * 50) % 2 > 1 ? 1 : -1) * Math.random() / 1000).toFixed(6);
+      //lat=0;
+      //lng=0;
+      var marker = {
+        id: "mk_" + i,
+        latitude: Number(center_lat) + Number(lat),
+        longitude: Number(center_lng) + Number(lng),
+        width: 40,
+        height: 40,
+        iconPath: "/images/icon/sos.png"
+      }
+      //console.log(JSON.stringify(marker));
+      markers.push(marker);
+    }
+
+    this.setData({
+      markers: markers
+    });
   }
 })
